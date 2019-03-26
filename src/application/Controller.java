1ef9bdeaef62da022d8883652f3b5526df8076a9
JackP2112 <jack6002@gmail.com>
2019-03-26 15:00:55 +0000

    Added start and destination selection.

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -3,12 +3,24 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class Controller {
+
+	@FXML TextField startField, destField;
+	private TextField selected;
 
 	@FXML
 	private void test(ActionEvent e){
 		Button btn = (Button) e.getSource();
-		System.out.println(btn.getLayoutX()+", "+btn.getLayoutY());
+		if(selected!=null) {
+            if (selected.equals(startField)) startField.setText(btn.getText());
+            else if (selected.equals(destField)) destField.setText(btn.getText());
+        }
+	}
+
+	@FXML
+	private void selectField(){
+	    selected = (startField.isFocused())? startField : destField;
 	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -15,6 +15,8 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.sizeToScene();
         primaryStage.show();
     }
 
